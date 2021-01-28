a88dbe27eb2457c41ea3d94b75b5eda91c8c8cb1
Artur Dvorak <artur.dvorak@gmail.com>
2021-01-28 15:10:55 -0500

    Adding End-to-End scenario

--- src/test/java/tests/SuiteEndToEndTest.java
+++ src/test/java/tests/SuiteEndToEndTest.java
@@ -11,7 +11,7 @@ public class SuiteEndToEndTest extends BaseTest{
     private String projectCode;
     private int caseId;
 
-    @Test(description = "Create new project, new suite and new test case", groups = "creation", enabled = true)
+    @Test(description = "Create new project, new suite and new test case", enabled = true) //update name
     public void createNewTestSuiteAndTestCase() {
         projectCode = projectAdapter.addProject(projectFactory.getProject());
 
@@ -25,10 +25,7 @@ public class SuiteEndToEndTest extends BaseTest{
         caseAdapter.addCase(testCase, projectCode);
         Case actualTestCase = caseAdapter.getCase(projectCode, caseId);
         Assert.assertEquals(actualTestCase, testCase);
-    }
 
-    @Test(description = "Update created test suite", groups = "updating", dependsOnGroups = "creation", enabled = true) //Test case can't be update through API
-    public void updateCreatedSuite() {
         Suite suiteToPut = Suite.builder()
                 .id(suiteId)
                 .title(RandomStringUtils.randomAlphabetic(10))
@@ -39,10 +36,7 @@ public class SuiteEndToEndTest extends BaseTest{
         suiteAdapter.updateSuite(projectCode, suiteId, suiteToPut);
         Suite updatedSuite = suiteAdapter.getSuite(projectCode, suiteId);
         Assert.assertEquals(updatedSuite, suiteToPut);
-    }
 
-    @Test(description = "Delete created project", dependsOnGroups = "updating", enabled = true)
-    public void deleteCreatedProject() {
         caseAdapter.deleteCase(projectCode, caseId);
         suiteAdapter.deleteSuite(projectCode, suiteId);
         //can't find API for project deleting
